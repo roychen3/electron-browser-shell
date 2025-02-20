a481c39f19dc4bc07c9d981174bd5c6e93894fc1
Roy Chen <roy850323@gmail.com>
2025-02-20 14:38:11 +0800

    Add typescript in electron

---
+++ src/main/constants.ts
@@ -0,0 +1,1 @@
+export const BROWSER_SHELL_DEV_URL = 'http://localhost:3000/'
---
+++ src/main/index.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+import { BROWSER_SHELL_DEV_URL} from './constants'
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadURL(BROWSER_SHELL_DEV_URL);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
