040852a2f1d78fd5d3bcf9992e589121266a832b
Roy Chen <roy850323@gmail.com>
2025-02-21 12:51:51 +0800

    Add `Toggle Browser Shell DevTools` in menu

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -2,7 +2,7 @@ import { WebContentsView, Menu } from 'electron';
 import { BROWSER_SHELL_DEV_URL, AUTHENTICATOR_DEV_URL } from './constants';
 import path from 'path';
 
-export function createApplicationMenu(browserContentView: WebContentsView) {
+export function createApplicationMenu(browserContentView: WebContentsView, browserShellView: WebContentsView) {
   const menu = Menu.getApplicationMenu();
   if (!menu) return;
 
@@ -15,5 +15,16 @@ export function createApplicationMenu(browserContentView: WebContentsView) {
     devToolsItem.click = () => browserContentView.webContents.toggleDevTools();
   }
 
-  Menu.setApplicationMenu(menu);
+  let template = Menu.buildFromTemplate(menu.items)
+  const localDeveloperMenu = menu.items.find(item => item.label === 'Local Developer');
+  if (!localDeveloperMenu) {
+    template = Menu.buildFromTemplate([...menu.items, {
+      label: 'Local Developer',
+      submenu: [{
+        click: () => browserShellView.webContents.toggleDevTools(),
+        label: 'Toggle Browser Shell DevTools',
+      }],
+    }]);
+  }
+  Menu.setApplicationMenu(template);
 }
