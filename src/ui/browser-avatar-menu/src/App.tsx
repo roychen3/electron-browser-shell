6a03889992144091a49e58e9a099ea6f75fdb19d
Roy Chen <roy850323@gmail.com>
2025-03-04 16:36:36 +0800

    [authenticator] add signed in title

--- src/ui/browser-avatar-menu/src/App.tsx
+++ src/ui/browser-avatar-menu/src/App.tsx
@@ -24,12 +24,15 @@ function App() {
     <div className="w-48 bg-neutral-800">
       <div className="py-2">
         {token ? (
-          <button
-            onClick={handleSignOut}
-            className="block w-full px-4 py-1 text-left text-sm text-white hover:bg-neutral-600"
-          >
-            Sign Out
-          </button>
+          <>
+            <div className="px-4 py-1 text-left text-sm text-white">Hi!</div>
+            <button
+              onClick={handleSignOut}
+              className="block w-full px-4 py-1 text-left text-sm text-white hover:bg-neutral-600"
+            >
+              Sign Out
+            </button>
+          </>
         ) : (
           <button
             onClick={handleSignIn}
