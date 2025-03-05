1873267d20ac45fe2e006af2f135d9de071810b6
Roy Chen <roy850323@gmail.com>
2025-03-05 14:49:07 +0800

    [authenticator] adjust auth redirect url

--- src/ui/authenticator/src/components/AuthGuard/index.tsx
+++ src/ui/authenticator/src/components/AuthGuard/index.tsx
@@ -1,4 +1,4 @@
-import { useLocation, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useAuth } from '../../auth/auth-react';
 
 export interface AuthGuardProps {
@@ -6,10 +6,18 @@ export interface AuthGuardProps {
 }
 export default function AuthGuard({ children }: AuthGuardProps) {
   const { token } = useAuth();
-  const { pathname } = useLocation();
 
-  if (pathname === '/sign-in' && token) {
-    return <Navigate to="/sign-up" />;
+  useEffect(() => {
+    if (token) {
+      const url = import.meta.env.PROD
+        ? 'app://protected-application'
+        : 'http://localhost:3010';
+      window.electronAPI.browserNavigateTo(url);
+    }
+  }, [token]);
+
+  if (token) {
+    return <div>Redirecting...</div>;
   }
   return <>{children}</>;
 }
