05155231045f884445a6016e5dde752fe3ebb041
Roy Chen <roy850323@gmail.com>
2025-03-07 17:17:27 +0800

    [browser-operations-panel] refactor tab style

--- src/ui/browser-operations-panel/src/components/TabBar/index.tsx
+++ src/ui/browser-operations-panel/src/components/TabBar/index.tsx
@@ -24,42 +24,59 @@ export default function TabBar({
   addNewTab,
 }: TabBarProps) {
   const isMacOS = window.electronAPI.isMacOS();
-  const barPadding = isMacOS ? 'pl-20' : 'pr-30';
+  const barPadding = isMacOS ? 'pl-20 pr-10' : 'pr-30';
+  const activeIndex = tabs.findIndex((tab) => tab.id === activeTabId);
+
   return (
     <div
       className={`flex items-center px-1 pt-1 bg-neutral-800 max-w-full ${barPadding} ${styles['electron-draggable']}`}
     >
-      <div className="flex flex-nowrap min-w-0 overflow-x-auto">
-        {tabs.map((tab) => (
-          <div
-            key={tab.id}
-            onClick={() => switchTab?.(tab.id)}
-            className={`group flex items-center min-w-[100px] max-w-[200px] w-[200px] px-4 py-1 mr-1 rounded-t-lg select-none ${
-              activeTabId === tab.id
-                ? 'bg-neutral-700 text-white'
-                : 'bg-neutral-900 text-gray-400 hover:bg-neutral-800'
-            } ${styles['electron-not-draggable']}`}
-          >
-            <span className="flex-1 truncate text-sm">
-              {tab.title || 'New Tab'}
-            </span>
-            <button
-              onClick={(event) => {
-                event.stopPropagation();
-                closeTab?.(tab.id);
-              }}
-              className="p-1 rounded-full opacity-0 group-hover:opacity-100 hover:bg-neutral-600"
+      <div className={`pr-[6px] flex flex-nowrap gap-[6px] min-w-0 overflow-x-hidden ${styles['electron-not-draggable']}`}>
+        {tabs.map((tab, tabIndex) => {
+          const isActive = activeTabId === tab.id;
+          const colorClasses = isActive
+            ? 'bg-neutral-700 text-white after:hidden'
+            : 'text-gray-400 hover:bg-neutral-900';
+
+          const titleOverflowClasses = isActive ? 'overflow-hidden' : '';
+
+          const isActivePrev = activeIndex - tabIndex === 1;
+          const dividerClasses = isActivePrev
+            ? ''
+            : 'after:absolute after:right-[-4px] after:top-[50%] after:transform-[translateY(-50%)] after:w-[2px] after:h-[60%] after:bg-neutral-700';
+
+          return (
+            <div
+              key={tab.id}
+              onClick={() => switchTab?.(tab.id)}
+              className={`group relative min-w-[34px] max-w-[200px] w-[200px] px-2 py-1 rounded-t-lg select-none ${colorClasses} ${dividerClasses}`}
             >
-              <IoClose size={14} />
-            </button>
-          </div>
-        ))}
+              <div className={`flex items-center overflow-hidden`}>
+                <span
+                  className={`flex-1 text-sm text-nowrap ${titleOverflowClasses}`}
+                >
+                  {tab.title || 'New Tab'}
+                </span>
+                <button
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    closeTab?.(tab.id);
+                  }}
+                  className={`p-0.5 rounded-full hover:bg-neutral-600`}
+                >
+                  <IoClose size={14} />
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
+
       <button
         onClick={addNewTab}
-        className={`p-1 text-white rounded-full hover:bg-neutral-600 ${styles['electron-not-draggable']}`}
+        className={`p-1 text-white rounded-full hover:bg-neutral-600 relative ${styles['electron-not-draggable']} `}
       >
-        <IoAdd size={20} />
+        <IoAdd size={14} />
       </button>
     </div>
   );
